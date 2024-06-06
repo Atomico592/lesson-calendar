29d07a3e84c73cd0a872ff4aa5d1c2f4d933acce
Atomico592 <atomico592@gmail.com>
2024-06-06 13:12:08 +0600

    Create and setup a project, install redux and sass, delete unnecessary files

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+const app = (
+    <App/>
+)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(app)
